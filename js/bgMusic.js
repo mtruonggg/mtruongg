3da4be321676211ebee3745495e42c3117941653
mtruonggg <minhtruongle181209@gmail.com>
2026-01-04 14:13:25 +0700

    Add files via upload
    Signed-off-by: mtruonggg <minhtruongle181209@gmail.com>

--- js/bgMusic.js
+++ js/bgMusic.js
@@ -1,1 +1,44 @@
+(() => {
+  const music = new Audio('https://files.catbox.moe/42piuv.mp3'); // 🔴 đổi link catbox tại đây
+  music.loop = true;
+  music.volume = 0.5;
 
+  let isPlaying = false;
+
+  // Tự phát nhạc sau khi vào màn pháo hoa
+  setTimeout(() => {
+    const tryPlay = () => {
+      music.play().then(() => {
+        isPlaying = true;
+      }).catch(() => {
+        // Chờ user click lần đầu (fix autoplay mobile)
+        document.addEventListener('click', () => {
+          if (!isPlaying) {
+            music.play();
+            isPlaying = true;
+          }
+        }, { once: true });
+      });
+    };
+    tryPlay();
+  }, 2000);
+
+  // Gắn vào nút sound có sẵn
+  setTimeout(() => {
+    const soundBtn = document.querySelector('.sound-btn');
+    const icon = soundBtn?.querySelector('use');
+
+    if (!soundBtn) return;
+
+    soundBtn.addEventListener('click', () => {
+      if (music.paused) {
+        music.play();
+        icon?.setAttribute('href', '#icon-sound-on');
+      } else {
+        music.pause();
+        icon?.setAttribute('href', '#icon-sound-off');
+      }
+    });
+  }, 1500);
+
+})();
